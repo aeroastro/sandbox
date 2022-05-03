842979890a2fbda22bb1f8ac6a7e7f96bffbf37a
Takumasa Ochi <aeroastro007@gmail.com>
2022-05-03 16:44:57 +0900

    Take input as a line not as a word

--- cmake/cmake-tutorial/src/console.cpp
+++ cmake/cmake-tutorial/src/console.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "Input anything." << std::endl;
 
     std::string input_line;
-    while(std::cin >> input_line) {
+    while(std::getline(std::cin, input_line)) {
         std::cout << "Your input is: " << input_line << std::endl;
     }
 
