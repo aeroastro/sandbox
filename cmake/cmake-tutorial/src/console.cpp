f6e1de7f18b5801229b2edba9c1d233d25959549
Takumasa Ochi <aeroastro007@gmail.com>
2022-05-03 16:16:21 +0900

    Add trivial echo command

---
+++ cmake/cmake-tutorial/src/console.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Input anything." << std::endl;
+
+    std::string input_line;
+    while(std::cin >> input_line) {
+        std::cout << "Your input is: " << input_line << std::endl;
+    }
+
+    std::cout << "Good-Bye!" << std::endl;
+}
