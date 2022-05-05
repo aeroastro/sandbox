ea37892a75f8231d92e6841edb602861dc5cf7d4
Takumasa Ochi <aeroastro007@gmail.com>
2022-05-05 17:49:33 +0900

    Add pragma once to prevent multiple load

--- cmake/cmake-tutorial/config.in.hpp
+++ cmake/cmake-tutorial/config.in.hpp
@@ -1,2 +1,5 @@
+// Clang, GCC, and MSVC supports this. Maybe OK? https://en.wikipedia.org/wiki/Pragma_once
+#pragma once
+
 #define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR@
 #define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR@
