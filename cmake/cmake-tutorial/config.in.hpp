aa83d7e13a236816f53e2a693b8f7818084f466f
Takumasa Ochi <aeroastro007@gmail.com>
2022-05-07 18:14:40 +0900

    Implement version in a C++-like way

--- cmake/cmake-tutorial/config.in.hpp
+++ cmake/cmake-tutorial/config.in.hpp
@@ -1,5 +1,46 @@
 // Clang, GCC, and MSVC supports this. Maybe OK? https://en.wikipedia.org/wiki/Pragma_once
 #pragma once
 
-#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR@
-#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR@
+#define Tutorial_VERSION_MAJOR "@Tutorial_VERSION_MAJOR@"
+#define Tutorial_VERSION_MINOR "@Tutorial_VERSION_MINOR@"
+
+#include <string>
+#include <iostream>
+#include <charconv>
+#include <utility>
+
+// This is header-only
+
+namespace Config {
+
+    struct Version {
+        int major;
+        int minor;
+
+        // NOTE: implicit conversions are not recommended.
+        // https://google.github.io/styleguide/cppguide.html#Implicit_Conversions
+        operator double() const {
+            return 1.0 * major + 0.001 * minor;
+        }
+
+        operator std::string() const {
+            return std::to_string(major) + "." + std::to_string(minor);
+        }
+    };
+
+    static Version GetVersion() {
+        std::string majorS = Tutorial_VERSION_MAJOR;
+        std::string minorS = Tutorial_VERSION_MINOR;
+
+        // locale-independent and exception-free (C++17)
+        // I might be a little bit too nervous about locale problems and exceptions...
+        // https://cpprefjp.github.io/reference/charconv/from_chars.html
+        int major;
+        int minor;
+        std::from_chars(majorS.data(), majorS.data() + majorS.size(), major);
+        std::from_chars(minorS.data(), minorS.data() + minorS.size(), minor);
+
+        Version version{major, minor};
+        return version;
+    }
+}
--- cmake/cmake-tutorial/console.cpp
+++ cmake/cmake-tutorial/console.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
+
 #include "config.hpp"
 
 int main() {
     std::cout << "Console Apps" << std::endl;
-    std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Input anything." << std::endl;
 
+    // implicit conversion and structured binding (C++17)
+    Config::Version version = Config::GetVersion();
+    std::string version_str = version;
+    double version_double = version;
+    auto [version_major, version_minor] = version;
+
+    std::cout
+        << "Version: " << version.major << "." << version.minor << std::endl
+        << "Version: " << version_str << std::endl
+        << "Version: " << version_double << std::endl;
+
+    std::cout << "Input anything." << std::endl;
     std::string input_line;
     while(std::getline(std::cin, input_line)) {
         std::cout << "Your input is: " << input_line << std::endl;
